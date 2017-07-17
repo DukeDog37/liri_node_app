29b2dcf3bca61aa0a28d2f5830244c1b8148e48f
Jason H <jahenry@gwmail.gwu.edu>
2017-07-16 20:48:48 -0400

    woriking user prompt

--- liri.js
+++ liri.js
@@ -1,47 +1,86 @@
-//Start with step 7 of the homework instructions
 
-
+//Use inquirer to prompt for user actions
+var inquirer = require("inquirer");
+var callAction = "";
 //8. Make it so liri.js can take in one of the following commands:
+var nodeArgs = process.argv;
+inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "username"
+    },
+    {
+      type: "checkbox",
+      message: "What would you like to do?",
+      choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'],
+      name: "reqaction"
+    },
+    // Here we ask the user to confirm.
+    {
+      type: "confirm",
+      message: "Are you sure:",
+      name: "confirm",
+      default: true
+    }    
+  ]).then(function(inquirerResponse) {
+    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+    if (inquirerResponse.confirm == true) {
+      callAction = inquirerResponse.reqaction;
+      console.log(inquirerResponse.reqaction);
+      console.log(callAction);
 
-//   * `my-tweets`
+			switch(inquirerResponse.reqaction[0])
+			{
 
-//   * `spotify-this-song`
+				case "my-tweets":
+					//console.log("my-tweets");
+					//* This will show your last 20 tweets and when they were created at in your terminal/bash window.
+					fnGetTweets(inquirerResponse.username);
+					break;
+				case "spotify-this-song":
+					console.log("spotify-this-song");
+					inquirer
+						  .prompt([
+						    // Here we create a basic text prompt.
+						    {
+						      type: "input",
+						      message: "What Song?",
+						      name: "songname"
+						    }
+						    ]).then(function(inquirerResponse) {
+						    	//console.log(inquirerResponse.songname)
+						    	fnSpotify(inquirerResponse.songname);
+						    });
+					
+					
+					break;
+				case "movie-this":
+					//console.log("movie-this");
+					fnGetMovieInfo();
+					
+					break;
+				case "do-what-it-says":
+					console.log("do-what-it-says");
+					
+					break;
+				
+				default:
+					console.log("Did not recognize the command");
+					break;
 
-//   * `movie-this`
-
-//   * `do-what-it-says`
-switch(process.argv[2])
-{
-
-	case "my-tweets":
-		//console.log("my-tweets");
-		//* This will show your last 20 tweets and when they were created at in your terminal/bash window.
-		fnGetTweets();
-		break;
-	case "spotify-this-song":
-		console.log("spotify-this-song");
-		fnSpotify();
-		
-		break;
-	case "movie-this":
-		//console.log("movie-this");
-		fnGetMovieInfo();
-		
-		break;
-	case "do-what-it-says":
-		console.log("do-what-it-says");
-		
-		break;
-	
-	default:
-		console.log("Did not recognize the command");
-		break;
-
-}
+			}
+		}
+		else {
+		      console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
+		    }
+	});
 
 
-function fnGetTweets(){
-
+function fnGetTweets(username){
+	var name = username;
 	var Twitter = require('twitter');
  	var myTwitterKeys = require("./keys.js");
 	var keys = myTwitterKeys.twitterKeys;
@@ -94,6 +133,10 @@ function fnGetMovieInfo(){
 			
 		}
 	}
+	//Sets Movie default to 'Mr. Nobody' in case one was not entered
+	if (movieName == ""){
+		movieName = "Mr. Nobody";
+	}
 		//console.log(movieName);
 		var request = require("request");
 		// Then run a request to the OMDB API with the movie specified
@@ -131,8 +174,10 @@ function fnGetMovieInfo(){
 
 }
 
-function fnSpotify(){
-	
+function fnSpotify(songname){
+	var nodeArgs = process.argv;
+	var SongName = songname;
+	//console.log("got herer : " + songname);
  	var mySpotKeys = require("./keys.js");
 	var keys = mySpotKeys.spotifyKeys;
 	var Spotify = require('node-spotify-api');
@@ -142,19 +187,24 @@ function fnSpotify(){
 	  secret: keys['client_secret']
 	});
  
-	spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+	spotify.search({ type: 'track', query: SongName }, function(err, data) {
 		  if (err) {
 		    return console.log('Error occurred: ' + err);
 		  }
 		  else{
 		  	console.log(data);
 		  	var firstPage = data.tracks.items;
-  			//console.log('The tracks in the first page are.. (popularity in parentheses)');
-
+  			//console.log(firstPage);
+  			//var rrr = JSON.stringify(firstPage);
+  			//console.log(rrr);
 			firstPage.forEach(function(track, index) {
-			    //console.log(track.album);
+			    console.log(index);
+			    //console.log(track.album.artists[index].name);
 			    //console.log(track.artists[index].name);
-			    console.log(index + ': ' + track.name + track.preview_url);
+			    //console.log(index + ': Artist: '+ track.album.artists[index].name + 
+			    //	" Track Name: " + track.name + 
+			    //	" Preview Here: " + track.preview_url);
+			    console.log(track.href);
 			  });
 			}
  	});
